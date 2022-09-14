f0444374fa56d4aa2473b79e663f7d14ed94f94f
Julien <jfustier42@gmail.com>
2022-09-14 13:44:32 +0200

    fix(API): Import AuthController on api.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\AdminQuestionController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\client\ClientQuestionController;
 use App\Http\Controllers\SurveyedController;
 use Illuminate\Http\Request;
