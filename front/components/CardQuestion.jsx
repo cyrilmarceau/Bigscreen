f2a3daeae4a894fe24f536a10733f5585a5cd079
cyrilmarceau <marceau.cyril@hotmail.fr>
2022-10-04 11:52:15 +0200

    refactor(FRONT): Form style to CardQuestion

--- front/components/CardQuestion.jsx
+++ front/components/CardQuestion.jsx
@@ -7,10 +7,10 @@ const CardQuestion = ({ title, content, span = 24, children }) => {
         <Col xs={span} md={14} lg={14} xl={14} xxl={14} justify='center' align='start' style={{ margin: 'auto' }}>
             <Card
                 title={`Question ${title}`}
-                headStyle={{ fontWeight: 'bold' }}
+                headStyle={{ fontWeight: 'bold', fontSize: '1rem' }}
                 bordered
                 style={{ borderRadius: 20, marginBottom: '25px', marginTop: '25px' }}>
-                <Meta title={`${content}`} />
+                <Meta title={`${content}`} style={{ marginBottom: 5 }} />
                 {children}
             </Card>
         </Col>
--- front/components/surveyedForm.jsx
+++ front/components/surveyedForm.jsx
@@ -1,101 +1,88 @@
 import { Button, Col, Form, Input, InputNumber, Row, Select, Radio } from 'antd';
 import React, { useId } from 'react';
+import API from '~/api';
+import CardQuestion from './CardQuestion';
 
-const SurveyedForm = ({ questions, submitSurveyed, submitFailed, loading }) => {
-
+const SurveyedForm = ({ questions, submitSurveyed, submitFailed }) => {
     const mapId = useId();
 
     const renderInput = (question) => {
-
         if (question.type === 'A') {
-
             const answerOptions = JSON.parse(question.options);
 
-            if(answerOptions.length === 2) {
-
+            if (answerOptions.length === 2) {
                 return (
                     <Radio.Group>
                         {answerOptions.map((answerOption, index) => (
-                            <Radio value={answerOption.key} key={`${mapId}-${index}`}>{answerOption.value}</Radio>
+                            <Radio value={answerOption.key} key={`${mapId}-${index}`}>
+                                {answerOption.value}
+                            </Radio>
                         ))}
                     </Radio.Group>
-                )
+                );
             }
 
-            return (               
+            return (
                 <Select placeholder='Sélectionner une réponse'>
-
                     {answerOptions.map((answerOption, index) => (
-
                         <Select.Option value={answerOption.key} key={`${mapId}-${index}`}>
                             {answerOption.value}
                         </Select.Option>
                     ))}
                 </Select>
             );
-        } 
-        
-        else if (question.type === 'B') {
-
-            return <Input maxLength={255} />;
-        } 
-        
-        else {
-
-            return <InputNumber type='number' min='1' max='5' />;
+        } else if (question.type === 'B') {
+            return <Input type='text' maxLength={255} />;
+        } else {
+            return <InputNumber defaultValue='1' type='number' min='1' max='5' />;
         }
     };
 
     const getValidationRules = (question) => {
-        
         let validationRules = [{ required: true, message: 'Votre réponse est requise' }];
 
-        if(question.id === 1) {
-
-            validationRules.push({ type: 'email', message: "Veuillez rentrer une adresse mail valide" });
+        if (question.id === 1) {
+            validationRules.push({ type: 'email', message: 'Veuillez rentrer une adresse mail valide' });
         }
 
-        if(question.type === "C") {
-            validationRules.push({ type: 'number', min: 1, max: 5, message: "Veuillez rentrer un nombre compris entre 1 et 5" });
+        if (question.type === 'C') {
+            validationRules.push({ type: 'number', min: 1, max: 5, message: 'Veuillez rentrer un nombre compris entre 1 et 5' });
         }
 
         return validationRules;
-    }
+    };
 
     return (
         <div className='surveyed-form'>
             <>
                 <Col span={20} justify='center' align='middle' className='card header-card'>
-
                     <h1 className='surveyed-form-title'>Big Screen</h1>
 
                     <p className='surveyed-form-paragraph'>Merci de répondre à toutes les questions et de valider le formulaire en bas de page</p>
                 </Col>
 
-                <Form initialValues={{ remember: true }} layout='vertical' onFinish={submitSurveyed} onFinishFailed={submitFailed} requiredMark={false} >
-
+                <Form
+                    initialValues={{ remember: true }}
+                    layout='vertical'
+                    onFinish={submitSurveyed}
+                    onFinishFailed={submitFailed}
+                    requiredMark={false}>
                     {questions.map((question, index) => (
-
-                        <Row justify='center' key={`${mapId}-${index}`}>
-
-                            <Col span={20} justify='center' align='middle' className='card question-card'>
-
-                                <h2 className="question-title">Question {question.title} :</h2>
-
+                        <Row key={index}>
+                            <CardQuestion title={question.title} content={question.content} key={index}>
                                 <Form.Item
                                     align='middle'
-                                    label={<label className="question-label">{question.content}</label>}
                                     name={index}
                                     extra={question.type === 'B' ? '255 caractères maximum' : ''}
                                     rules={getValidationRules(question)}>
                                     {renderInput(question)}
                                 </Form.Item>
-                            </Col>
+                            </CardQuestion>
                         </Row>
                     ))}
 
                     <Form.Item wrapperCol={{ span: 24 }}>
-                        <Button className='surveyed-form-submit' type='primary' htmlType='submit' loading={loading}>
+                        <Button className='surveyed-form-submit' type='primary' htmlType='submit'>
                             Envoyer
                         </Button>
                     </Form.Item>
